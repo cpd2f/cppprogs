c5beafb1631d20d9e8a30acafd4dcafa9889c019
Kaito Luijbregts <kluijbregtsk@gmail.com>
2025-06-02 10:55:00 +0200

    Hello World program

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// header file library that lets us work with input and output objects
+#include <iostream>
+
+// entry point for C and C++ programs
+int main() {
+
+  // prints a string of characters into the screen, in this case "Hello world!", std::cout indicates that cout is in the standard (std) namespace, endl indicates the line ends
+  std::cout << "Hello world!" << endl;
+  
+  // terminates a program successfully
+  return 0;
+}
